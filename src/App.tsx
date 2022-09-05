d649da6a98e2914078ad2581d6112aeeed12af3f
Aditya <aditya.gupta@thinkitive.com>
2022-09-05 20:34:42 +0530

    feat : Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/UI/Navbar";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Box } from "@mui/system";
+import images from "./images/avatar.jpeg";
+import Avatar from "@mui/material/Avatar";
+
 const App: React.FC = () => {
-  return <div className="App">HelLo World</div>;
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Navbar
+        name="Aditya Gupta"
+        icon={<MenuIcon />}
+        profileIcon={
+          <Avatar
+            alt="Cindy Baker"
+            src={images}
+            sx={{ width: 50, height: 50 }}
+          />
+        }
+      />
+    </Box>
+  );
 };
 
 export default App;
---
+++ src/components/UI/Navbar.tsx
@@ -0,0 +1,167 @@
+import * as React from "react";
+import { Stack } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import { Button } from "@mui/material";
+
+const Navbar: React.FC<Props> = ({ name, icon, profileIcon }) => {
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+    React.useState<null | HTMLElement>(null);
+
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          {profileIcon}
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            {icon}
+          </IconButton>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex", fontSize: "2.5rem" } }}>
+            <IconButton
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+            >
+              <Badge badgeContent={4} color="error">
+                <MailIcon fontSize="large" />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon fontSize="large" />
+              </Badge>
+            </IconButton>
+            <Box>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <Badge badgeContent={""} color="success" overlap="circular">
+                  {profileIcon}
+                </Badge>
+              </IconButton>
+            </Box>
+            <Stack sx={{ marginLeft: "1rem", height: "50%", marginTop: "5px" }}>
+              <Typography>{name}</Typography>
+              <Button sx={{ color: "white", fontSize: "12px" }}>
+                View Profile
+              </Button>
+            </Stack>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+    </Box>
+  );
+};
+
+interface Props {
+  name: string;
+  icon?: any;
+  profileIcon: any;
+}
+
+export default Navbar;
