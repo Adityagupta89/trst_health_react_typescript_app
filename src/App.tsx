cecb3df9b9e441fe7fa4a7847d46b448c507a8da
Aditya <aditya.gupta@thinkitive.com>
2022-09-05 20:38:18 +0530

    Feat : Add Sidebar Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,48 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/UI/Navbar";
 import MenuIcon from "@mui/icons-material/Menu";
+import Sidebar from "./components/UI/Sidebar";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 import { Box } from "@mui/system";
 import images from "./images/avatar.jpeg";
 import Avatar from "@mui/material/Avatar";
 
 const App: React.FC = () => {
+  const heading: any[] = [
+    {
+      name: "Dashboard",
+      icon: <InboxIcon />,
+    },
+    {
+      name: "Provider",
+      icon: <MailIcon />,
+    },
+    {
+      name: "Patients",
+      icon: <MailIcon />,
+    },
+    {
+      name: "Appointments",
+      icon: <InboxIcon />,
+    },
+    {
+      name: "Care Program",
+      icon: <InboxIcon />,
+    },
+    {
+      name: "Devices",
+      icon: <InboxIcon />,
+    },
+    {
+      name: "Settings",
+      icon: <InboxIcon />,
+    },
+    {
+      name: "SignOut",
+      icon: <InboxIcon />,
+    },
+  ];
   return (
     <Box sx={{ display: "flex" }}>
       <Navbar
@@ -20,6 +57,7 @@ const App: React.FC = () => {
           />
         }
       />
+      <Sidebar head={heading} />
     </Box>
   );
 };
